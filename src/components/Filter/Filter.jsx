0a1e677a1fe0c01ce3ba120decd9f0491fe8f811
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-10-04 18:38:47 +0330

    add option filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Filter.module.scss'
 const filterData = [
     { id: 1, title: 'allData' },
@@ -10,14 +10,29 @@ const Filter = ({ setFilter }) => {
         setFilter(title)
     }
 
+const[showfilter,setShowfilter]=useState('none')
+const[showIconBack , setShowIconBack]=useState('none')
+const[showIconfilter , setShowIconfilter]=useState('flex')
+const handleClickFilter =() =>{
+    setShowfilter('flex')
+    setShowIconBack('flex')
+    setShowIconfilter('none')
+
+}
+const handleDisFilter =()=>{
+    setShowfilter('none')
+    setShowIconBack('none')
+    setShowIconfilter('flex')
+}
     return (
         <div className={styles.container}>
-            <div className={styles.iconFilter}>
-            <i class="fa-solid fa-filter"></i>
+            <div className={styles.iconFilter} >
+                <i onClick={handleClickFilter} style={{display:showIconfilter}} class="fa-solid fa-filter"></i>
+                <i onClick={handleDisFilter} style={{display:showIconBack}} class="fa-solid fa-arrow-left"></i>
             </div>
             <div className={styles.filter}>
                 {filterData.map(filterBtn => (
-                    <p className={styles.childFilter}>
+                    <p className={styles.childFilter} style={{display:showfilter}}>
                         <input name='title' type={'radio'} onClick={() => handleFilterBtn(filterBtn.title)} />
                         {filterBtn.title}
                     </p>

e31ca82d5db69e501392fec5c2e390d3ada75025
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-10-05 20:14:45 +0330

    change style card visit

--- src/Page/SinglePageUser.jsx
+++ src/Page/SinglePageUser.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import styles from './SinglePage.module.scss'
+import Styles from './SinglePage.module.scss'
 const SinglePageUser = ({ todos }) => {
 
     const [contact, setContact] = useState({})
@@ -11,46 +11,48 @@ const SinglePageUser = ({ todos }) => {
     }, [])
 
     return (
-        <div  className={styles.container}>
-            <div>
-                <h5>
-                    <img style={{ width: '50px' }} src={`https://avatars.dicebear.com/api/avataaars/:${contact.user}.svg`} alt="avatar" />
-                </h5>
+        <div style={{display:'flex',justifyContent:'center'}}>
+            <div className={Styles.container}>
+                <Link to={'/'}>
+                    <button className='btn btn-primary'>
+                        <i class="fa-solid fa-arrow-left"></i>Home
+                    </button>
+                </Link>
+                <div class="row g-0 border rounded-2 m-3 ">
+                    <div className={Styles.Avatar} >
+                        <img style={{ width: '100' }} src={`https://avatars.dicebear.com/api/avataaars/:${contact.user}.svg`} alt="avatar" />
+                    </div>
+                    <div className={Styles.Card}>
+                        <div className={Styles.itemCard}>
+                            <h5>user:{contact.user}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard}>
+                            <h5>name : {contact.name}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard}>
+                            <h5>lastName:{contact.lastName}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard}>
+                            <h5> phone:{contact.phone}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard} style={{ cursor: 'pointer' }}>
+                            <h5>state:{contact.state ? ' Inactive ' : ' active '}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard}>
+                            <h5>age:{contact.age}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard}>
+                            <h5> Country:{contact.Country}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard}>
+                            <h5>City:{contact.City}</h5>
+                        </div>
+                        <div class="card-body" className={Styles.itemCard}>
+                            <h5> Email:<br />{contact.email}</h5>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div  className={styles.containerCard}>
-                <div >
-                    <h5>user: {contact.user}</h5>
-                </div>
-                <div>
-                    <h5>Name: {contact.name}</h5>
-                </div>
-                <div>
-                    <h5>lastName: {contact.lastName}</h5>
-                </div>
-                <div>
-                    <h5>phone: {contact.phone}</h5>
-                </div>
-                <div>
-                    <h5>state: {contact.state ? ' Inactive ' : ' active '}</h5>
-                </div>
-                <div>
-                    <h5>Age: {contact.age}</h5>
-                </div>
-                <div>
-                    <h5>Country: {contact.Country}</h5>
-                </div>
-                <div >
-                    <h5>City: {contact.City}</h5>
-                </div>
-                <div>
-                    <h5>Email: {contact.email}</h5>
-                </div>
-            </div>
-            <Link to={'/'}>
-                <button className='btn btn-primary'>
-                    <i class="fa-solid fa-arrow-left"></i>Home
-                </button>
-            </Link>
 
         </div>
 
